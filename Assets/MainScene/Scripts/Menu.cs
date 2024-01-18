3ede9d0231ab3d9547fa6395aec8d997cf691447
Jordji <37375293+goldenapple545@users.noreply.github.com>
2024-01-18 16:15:30 +0300

    Add axis transition

--- Assets/MainScene/Scripts/Menu.cs
+++ Assets/MainScene/Scripts/Menu.cs
@@ -6,10 +6,31 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject menuContent;
     [SerializeField] private Animator modelAnimator;
-    
+    [SerializeField] private OrbitCamera orbitCamera;
+
+    [SerializeField] private Transform defaultTarget;
+    [SerializeField] private Transform axisTarget;
+
+    private bool axisLook = false;
     public void MenuExpand()
     {
         menuContent.SetActive(!menuContent.activeSelf);
         modelAnimator.SetFloat("Expand", menuContent.activeSelf ? 1 : 0);
+    }
+
+    public void AxisTap()
+    {
+        axisLook = !axisLook;
+        modelAnimator.SetBool("AxisLook", axisLook);
+        
+        if (axisLook)
+        {
+            orbitCamera.target = axisTarget;
+        }
+        else
+        {
+            orbitCamera.target = defaultTarget;
+            axisTarget.localEulerAngles = new Vector3(0, 0 ,0);
+        }
     }
 }
